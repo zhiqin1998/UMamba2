f07a6dfbd214af92ea9334aac3f54d8faaeb5534
Zhi Qin Tan <zhiqin1998@hotmail.com>
2025-10-03 13:43:22 +0100

    bug fixes

--- nnunetv2/training/nnUNetTrainer/variants/data_augmentation/nnUNetTrainerNoMirroring.py
+++ nnunetv2/training/nnUNetTrainer/variants/data_augmentation/nnUNetTrainerNoMirroring.py
@@ -59,6 +59,20 @@ class nnUNetTrainer_onlyMirror01_1500ep(nnUNetTrainer_onlyMirror01):
         self.num_epochs = 1500
 
 
+class nnUNetTrainer_onlyMirror01_500ep(nnUNetTrainer_onlyMirror01):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict,
+                 device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, device)
+        self.num_epochs = 500
+
+
+class nnUNetTrainerNoMirroring_500ep(nnUNetTrainerNoMirroring):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict,
+                 device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, device)
+        self.num_epochs = 500
+        
+
 class nnUNetTrainer_onlyMirror01_DASegOrd0(nnUNetTrainer_onlyMirror01):
     @staticmethod
     def get_training_transforms(
