eb8c05a394d85a19bdedd871822157682dc4ae9d
Zhi Qin Tan <zhiqin1998@hotmail.com>
2026-02-23 09:50:32 +0000

    added ablation trainer

--- nnunetv2/training/nnUNetTrainer/variants/competitions/toothfairy3.py
+++ nnunetv2/training/nnUNetTrainer/variants/competitions/toothfairy3.py
@@ -970,6 +970,16 @@ class nnUNetTrainer_onlyMirror01_smoothLoss_weighted_nolr(nnUNetTrainer_onlyMirr
 
         return loss
 
+class nnUNetTrainer_pulpy_abl_accum2(nnUNetTrainer_onlyMirror01):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict,
+                 device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, device)
+        self.num_val_iterations_per_epoch = 10  # 50 # save some time
+        self.num_epochs = 300
+        self.accumulate_steps = 2
+        self.num_iterations_per_epoch = self.num_iterations_per_epoch * 2
+        self.oversample_foreground_percent = 0.66
+
 class nnUNetTrainer_LRMirror_TF3_abl_accum2(nnUNetTrainer_LRMirror):
     def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict,
                  device: torch.device = torch.device('cuda')):
@@ -1174,6 +1184,94 @@ class nnUNetTrainer_TF3_abl_accum2(nnUNetTrainer_TF3_Task1):
         self.num_epochs = 300
         self.accumulate_steps = 2
         self.num_iterations_per_epoch = self.num_iterations_per_epoch * 2
+
+class nnUNetTrainer_TF3_abl_accum2_smooth(nnUNetTrainer_TF3_Task1):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict,
+                 device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, device)
+        self.num_val_iterations_per_epoch = 10  # 50 # save some time
+        self.num_epochs = 300
+        self.accumulate_steps = 2
+        self.num_iterations_per_epoch = self.num_iterations_per_epoch * 2
+
+    def _build_loss(self):
+        if self.label_manager.has_regions:
+            raise NotImplementedError
+        else:
+            loss = DC_and_CE_smooth_loss({'batch_dice': self.configuration_manager.batch_dice,
+                                         'smooth': 1e-5, 'do_bg': False, 'ddp': self.is_ddp}, {}, weight_ce=1, weight_dice=1,
+                                         ignore_label=self.label_manager.ignore_label, dice_class=MemoryEfficientSoftDiceLoss)
+
+        if self._do_i_compile():
+            loss.dc = torch.compile(loss.dc)
+
+        # we give each output a weight which decreases exponentially (division by 2) as the resolution decreases
+        # this gives higher resolution outputs more weight in the loss
+
+        if self.enable_deep_supervision:
+            deep_supervision_scales = self._get_deep_supervision_scales()
+            weights = np.array([1 / (2 ** i) for i in range(len(deep_supervision_scales))])
+            if self.is_ddp and not self._do_i_compile():
+                # very strange and stupid interaction. DDP crashes and complains about unused parameters due to
+                # weights[-1] = 0. Interestingly this crash doesn't happen with torch.compile enabled. Strange stuff.
+                # Anywho, the simple fix is to set a very low weight to this.
+                weights[-1] = 1e-6
+            else:
+                weights[-1] = 0
+
+            # we don't use the lowest 2 outputs. Normalize weights so that they sum to 1
+            weights = weights / weights.sum()
+            # now wrap the loss
+            loss = DeepSupervisionWrapper(loss, weights)
+
+        return loss
+
+
+class nnUNetTrainer_TF3_abl_accum2_weighted(nnUNetTrainer_TF3_Task1):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict,
+                 device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, device)
+        self.num_val_iterations_per_epoch = 10  # 50 # save some time
+        self.num_epochs = 300
+        self.accumulate_steps = 2
+        self.num_iterations_per_epoch = self.num_iterations_per_epoch * 2
+
+    def _build_loss(self):
+        class_weight = [1] + [1] * 42 + \
+                       [10., 10., 10.] + [1]  # canals
+        assert len(class_weight) == 47
+        class_weight = torch.tensor(class_weight, device=self.device)
+        if self.label_manager.has_regions:
+            raise NotImplementedError
+        else:
+            loss = DC_and_CE_loss({'batch_dice': self.configuration_manager.batch_dice,
+                                   'smooth': 1e-5, 'do_bg': False, 'ddp': self.is_ddp},
+                                  {"weight": class_weight}, weight_ce=1, weight_dice=1,
+                                  ignore_label=self.label_manager.ignore_label, dice_class=MemoryEfficientSoftDiceLoss)
+
+        if self._do_i_compile():
+            loss.dc = torch.compile(loss.dc)
+
+        # we give each output a weight which decreases exponentially (division by 2) as the resolution decreases
+        # this gives higher resolution outputs more weight in the loss
+
+        if self.enable_deep_supervision:
+            deep_supervision_scales = self._get_deep_supervision_scales()
+            weights = np.array([1 / (2 ** i) for i in range(len(deep_supervision_scales))])
+            if self.is_ddp and not self._do_i_compile():
+                # very strange and stupid interaction. DDP crashes and complains about unused parameters due to
+                # weights[-1] = 0. Interestingly this crash doesn't happen with torch.compile enabled. Strange stuff.
+                # Anywho, the simple fix is to set a very low weight to this.
+                weights[-1] = 1e-6
+            else:
+                weights[-1] = 0
+
+            # we don't use the lowest 2 outputs. Normalize weights so that they sum to 1
+            weights = weights / weights.sum()
+            # now wrap the loss
+            loss = DeepSupervisionWrapper(loss, weights)
+
+        return loss
 
 
 class nnUNetTrainer_TF3_primus(nnUNetTrainer_warmup): # see ../../primus for reference
